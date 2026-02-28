9c76faa3d81a5f66f11c60c8b2753ba6722bbd7c
Fuxuanmylove <2793275118@qq.com>
2026-03-01 01:45:34 +0800

    创建了HUD类

---
+++ Source/GASTopDownRPG2/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// BakaMMGameMaker Learning From Druid Mechanics
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
---
+++ Source/GASTopDownRPG2/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,28 @@
+// BakaMMGameMaker Learning From Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class GASTOPDOWNRPG2_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> OverlayWidgetClass;
+};
