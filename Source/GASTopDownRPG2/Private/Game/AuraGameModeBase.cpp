d10119172eebe7c4289964f642f1cfbcb31d2e19
Fuxuanmylove <2793275118@qq.com>
2026-02-28 20:04:46 +0800

    添加游戏Mode类，添加相机控制，添加Hover高光接口

---
+++ Source/GASTopDownRPG2/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// BakaMMGameMaker Learning From Druid Mechanics
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/GASTopDownRPG2/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// BakaMMGameMaker Learning From Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTOPDOWNRPG2_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
