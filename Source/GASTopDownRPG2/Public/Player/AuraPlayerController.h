0e6fbd39695233e878a4bec568f1f8fd6b905250
Fuxuanmylove <2793275118@qq.com>
2026-02-28 19:30:39 +0800

    添加了移动函数

--- Source/GASTopDownRPG2/Public/Player/AuraPlayerController.h
+++ Source/GASTopDownRPG2/Public/Player/AuraPlayerController.h
@@ -7,6 +7,8 @@
 #include "AuraPlayerController.generated.h"
 
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -18,11 +20,17 @@ class GASTOPDOWNRPG2_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
-	
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
