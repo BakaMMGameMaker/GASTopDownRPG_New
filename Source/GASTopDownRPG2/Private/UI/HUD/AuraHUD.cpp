e607ddfa23f8722e8139300d96d45c7d105374f9
Fuxuanmylove <2793275118@qq.com>
2026-03-01 16:13:24 +0800

    完善了生命值的委托系统

--- Source/GASTopDownRPG2/Private/UI/HUD/AuraHUD.cpp
+++ Source/GASTopDownRPG2/Private/UI/HUD/AuraHUD.cpp
@@ -13,6 +13,7 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
 	{
 		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
 		OverlayWidgetController->SetWidgetControllerParams(WidgetControllerParams);
+		OverlayWidgetController->BindCallbacksToDependencies();
 	}
 	return OverlayWidgetController;
 }
@@ -28,6 +29,7 @@ void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySyst
 	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
 
 	OverlayWidget->SetWidgetController(WidgetController);
+	WidgetController->BroadcastInitialValues();
 
 	Widget->AddToViewport();
 }
--- Source/GASTopDownRPG2/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/GASTopDownRPG2/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,40 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// 广播给 Widgets
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+
+	const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(AttributeSet);
+
+	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
+
+void UOverlayWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+
+	const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(AttributeSet);
+
+	// 监听 Model 中的数值变化，GAS 内置的属性变化广播系统
+	// 与头文件中的自定义 delegates 不是一回事
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetHealthAttribute())
+	                      .AddUObject(this, &UOverlayWidgetController::HealthChanged);
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute())
+	                      .AddUObject(this, &UOverlayWidgetController::MaxHealthChanged);
+}
+
+void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
+{
+	OnHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxHealthChanged.Broadcast(Data.NewValue);
+}
