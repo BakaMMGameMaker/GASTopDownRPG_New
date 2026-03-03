6a92d1e54ce636793755525646b641472e1300a8
Fuxuanmylove <2793275118@qq.com>
2026-03-03 13:01:41 +0800

    添加了获取Actor等级的逻辑

---
+++ Source/GASTopDownRPG2/Public/Interaction/CombatInterface.h
@@ -0,0 +1,25 @@
+// BakaMMGameMaker Learning From Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CombatInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCombatInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class GASTOPDOWNRPG2_API ICombatInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual int32 GetPlayerLevel();
+};
